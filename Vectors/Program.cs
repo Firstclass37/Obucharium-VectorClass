e12efe72fb3ef6c8da048b9c812dedbe403f2071
Ivan <firstclass37@mail.ru>
2016-05-20 15:31:16 +0300

    First

--- Vectors/Program.cs
+++ Vectors/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,62 @@ namespace Vectors
     {
         static void Main(string[] args)
         {
+            List<Vector> vectors = new List<Vector>();
+            while (Console.ReadLine() !="stop")
+            {
+                Vector vector;
+                if (InputVectorDialog(out vector))
+                {
+                    vectors.Add(vector);
+                }
+                ShowResult(vectors);
+            }
+            vectors.Sort();
+            ShowResult(vectors);
+            Console.ReadKey();
+        }
+
+        private static bool InputVectorDialog(out Vector vector)
+        {
+            vector = null;
+            char first;
+            char second;
+            int x1;
+            int y1;
+            int x2;
+            int y2;
+            Console.Write("First point char:");
+            first = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.Write("First point char x:");
+            if (!int.TryParse(Console.ReadLine(), out x1)) return false;
+            Console.WriteLine();
+            Console.Write("First point char y:");
+            if (!int.TryParse(Console.ReadLine(), out y1)) return false;
+            Console.WriteLine();
+            Console.Write("Second point char:");
+            second = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.Write("Second point char x:");
+            if (!int.TryParse(Console.ReadLine(), out x2)) return false;
+            Console.WriteLine();
+            Console.Write("Second point char y:");
+            if (!int.TryParse(Console.ReadLine(), out y2)) return false;
+            Console.WriteLine();
+            vector = new Vector(first, second, x1, y1, x2, y2);
+            return true;
+
+        }
+
+        private static void ShowResult(IEnumerable<Vector> vectors )
+        {
+            Console.Clear();
+            Console.WriteLine(new string('=',15));
+            foreach (Vector vec in vectors)
+            {
+                Console.WriteLine(vec);
+            }
+            Console.WriteLine(new string('=', 15));
         }
     }
 }
---
+++ Vectors/Vector.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    class Vector:IComparable,IComparer<Vector>
+    {
+        public int Ax { get; private set; }
+        public int Ay { get; private set; }
+        public int Bx { get; private set; }
+        public int By { get; private set; }
+        public int Xa { get; private set; }
+        public int Ya { get; private set; }
+        public string Name { get; private set; }
+        public double Lenght { get; private set; }
+
+        public Vector(char one,char two,int ax, int ay,int bx, int by)
+        {
+            Ax = ax;
+            Ay = ay;
+            Bx = bx;
+            By = by;
+            Xa = Bx - Ax;
+            Ya = By - Ay;
+            Name = one.ToString() + two.ToString();
+            Lenght = Math.Sqrt(Math.Pow(Xa,2) + Math.Pow(Ya,2));
+        }
+      
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Vector)) return -1;
+            else
+            {
+                var tempVector = obj as Vector;
+                if (Lenght == tempVector.Lenght) return 0;
+                if (Lenght > tempVector.Lenght) return 1;
+                if (Lenght < tempVector.Lenght) return -1;
+                
+            }
+            return 0;
+
+        }
+
+        public int Compare(Vector one, Vector two)
+        {
+            if (one.Lenght == two.Lenght) return 0;
+            if (one.Lenght > two.Lenght) return 1;
+            if (one.Lenght < two.Lenght) return -1;
+            return 0;
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}({1},{2})",Name,Xa,Ya);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Ax*111 - Bx*111 + Bx*By%2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Vector)) return false;
+            else
+            {
+                var tempVector = obj as Vector;
+                return Lenght == tempVector.Lenght ? true : false;
+            }
+        }
+
+    }
+}
